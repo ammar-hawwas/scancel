016f7238b689d304ad01e3d9ef14314fc69d601d
ammar hawwas <amarhawass11@gmail.com>
2025-06-09 22:26:42 +0300

    Add files via upload

---
+++ scan-proj/README.txt
@@ -0,0 +1,7 @@
+pip install -r requirement.txt
+
+python app.py
+
+URL "https://127.0.0.1:5000"
+
+Enter PEM pass phrase : biba@92S56
---
+++ scan-proj/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
